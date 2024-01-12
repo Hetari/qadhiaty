457a27f8f14a6b8ff78db4d008340bdc3e7100ae
Hetari <hetari4py@gmail.com>
2024-01-12 23:49:35 +0300

    add process.env.PORT

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const {
 } = require("./database/db.js");
 // const client = new OFF();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const allProducts = getAllProducts();
 
